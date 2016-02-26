d6af5c156e1be053071f8839d898b429c3f05907
COMETE <jlemoine@u-paris10.fr>
2016-02-26 17:26:27 +0100

    Update version.php

--- version.php
+++ version.php
@@ -1,10 +1,10 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2014041801;    // The (date) version of this module + 2 extra digital for daily versions
+$plugin->version = 2016022601;    // The (date) version of this module + 2 extra digital for daily versions
                                   // This version number is displayed into /admin/forms.php
 $plugin->requires = 2013040500;   // Requires this Moodle version - at least 2.0
 $plugin->cron = 0;
 $plugin->component = 'local_groupautoenrol';
-$plugin->release = '1.1';
+$plugin->release = '1.1.1';
 $plugin->maturity = MATURITY_STABLE;
